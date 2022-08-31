419557b012a716e5c32feff8327b0204dc7dcf56
gabilopess9 <69826798+gabilopess9@users.noreply.github.com>
2022-08-30 21:48:27 -0300

    Create No.h

---
+++ No.h
@@ -0,0 +1,31 @@
+/** ******************************************************************
+ TAD Árvore BST
+ Estrutura de Dados II
+ GRUPO
+ Nome: Gabriela Lopes Francisco
+ TIA: 31744591
+ Nome: 
+ TIA: 
+
+****************************************************************** */
+
+#ifndef TAD_ARVORE_BST_NO_H
+#define TAD_ARVORE_BST_NO_H
+
+class No {
+
+private:
+    No* esq;
+    No* dir;
+
+public:
+    No(int);
+    int  getChave();
+    No* getEsq();
+    No* getDir();
+    void setEsq(No*);
+    void setDir(No*);
+
+    int chave;
+};
+#endif
