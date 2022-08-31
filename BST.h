53058c57869c76a60350e2009555f0eb1c7f5ac7
gabilopess9 <69826798+gabilopess9@users.noreply.github.com>
2022-08-30 21:46:13 -0300

    Create BST.h

---
+++ BST.h
@@ -0,0 +1,47 @@
+/** ******************************************************************
+ TAD Árvore BST
+ Estrutura de Dados II
+ GRUPO
+ Nome: Gabriela Lopes Francisco
+ TIA: 31744591
+ Nome: 
+ TIA: 
+
+****************************************************************** */
+
+using namespace std;
+
+class ArvoreBST {
+
+private:
+    No *raiz;
+
+public:
+    ArvoreBST();
+
+    No *getRaiz();
+
+    void insereElemento(int chave);
+
+    void insereAux(No *no, int chave);
+
+    void imprimeEmOrdem(No *no);
+
+    void imprimePreOrdem(No *no);
+
+    void imprimePosOrdem(No *no);
+
+    int imprimeAlturaArvore(No *no);
+
+    No *pesquisaElemento(No *no, int chave);
+
+    int contaNos(No *no);
+
+    int contaFolhas(No *no);
+
+    int valorMinimo(No *no);
+
+    int valorMaximo(No *no);
+
+    bool removeElemento(int chave);
+};
